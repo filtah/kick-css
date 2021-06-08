807da83c7e3a7722374b735e69de6b5d2c7359f2
filtah <filtah@outlook.com>
2021-06-08 03:36:48 +0200

    can handle multiple declaration in rule

--- main.js
+++ main.js
@@ -1,127 +1,173 @@
+/**
+ * in-browser 
+ * @param {*} s 
+ * @param {*} ssUrl 
+ */
 function kickCSS(s, ssUrl='') {
 
-    const selector = document.querySelector(s)
-    if ( ! selector ) return
+    main(s, ssUrl)
 
-    // if ( typeof window.kick_css_output === 'undefined' ) {
-    //     window.kick_css_output = '';
-    // }
-    // window.kick_css_output += 'x'
-    // let output = window.kick_css_output
+    function main(s, ssUrl) {
 
-    // sort the output by media query breakpoints.
-    // init the map with breakpoint keys..
-    const output = new Map([
-        [''    , {}],
-        ['sm:' , {}],
-        ['md:' , {}],
-        ['lg:' , {}],
-    ])
+        const selector = document.querySelector(s)
+        if ( ! selector ) return
+
+        // if ( typeof window.kick_css_output === 'undefined' ) {
+        //     window.kick_css_output = '';
+        // }
+        // window.kick_css_output += 'x'
+        // let output = window.kick_css_output
+
+        // sort the output by media query breakpoints.
+        // init the map with breakpoint keys..
+        const output = new Map([
+            [''    , {}],
+            ['sm:' , {}],
+            ['md:' , {}],
+            ['lg:' , {}],
+        ])
 
 
-    // init the output string..
-    let outputHTML = ''
-    let previewHTML = ''
+        // init the output string..
+        let outputHTML = ''
+        let previewHTML = ''
 
-    // external stylesheet index..
-    const ssIndex = getStyleSheetIndex(ssUrl)
-    // console.log({ssUrl})
-    // console.log({ssIndex})
+        // external stylesheet index..
+        const ssIndex = getStyleSheetIndex(ssUrl)
+        // console.log({ssUrl})
+        // console.log({ssIndex})
 
-    // query the dom..
-    const query = document.querySelectorAll('[class]')
-    query.forEach(function(item) {
+        // query the dom..
+        const query = document.querySelectorAll('[class]')
+        query.forEach(function(item) {
 
-        // console.log(item.className)
+            // console.log(item.className)
 
-        const regex = /(?<!\S)(sm:|md:|lg:)?(pt|pb|pl|pr|mt|mb|ml|mr|mnw|mxw)-(\d+)(\.\d+)?(rem|em|px)(?!\S)/g
+            const regex = /(?<!\S)(sm:|md:|lg:)?(p|pt|pb|pl|pr|px|py|m|mt|mb|ml|mr|mx|my|mnw|mxw)-(\d+)(\.\d+)?(rem|em|px)(?!\S)/g
 
-        const matches = [...item.className.matchAll(regex)]
-        
-        if ( matches.length ) {
+            const matches = [...item.className.matchAll(regex)]
             
-            // console.log( matches )
+            if ( matches.length ) {
 
-            matches.forEach(match => {
+                matches.forEach(match => {
 
-                // console.log(match)
+                    // console.log(match)
+                    const rule = composeRule(match)
 
-                let _rule = {
-                    selector: escapeSelector(match[0]),
-                    miniMQ:   match[1] || '',
-                    fullMQ:   lookupMQ(match[1]) || '',
-                    miniDec:  match[2],
-                    fullDec:  lookupProp(match[2]),
-                    whole:    match[3],
-                    fraction: match[4] || '',
-                    unit:     match[5]
-                }
-                // console.log(_rule)
-                
-                let mq = output.get(_rule.miniMQ)
+                    let mq = output.get(rule.mq)
+                    mq[rule.selector] = rule.block
+                    output.set(rule.mq, mq)
 
-                mq['.'+_rule.selector] = `{ ${_rule.fullDec}: ${_rule.whole}${_rule.fraction}${_rule.unit}; }`
-
-                output.set(_rule.miniMQ, mq)
-
-            })
-        }
-    });
-
-    // build the output..
-    output.forEach((rules, mq) => {
-
-        // console.log(mq, rules)
-
-        // no media query..
-        if ( !mq && Object.entries(rules).length ) {
-
-            for (const sel in rules) {
-
-                if ( !hasRule(sel) ) {
-                    previewHTML += `${sel} ${rules[sel]}\n`
-                }
-
-                outputHTML += `${sel} ${rules[sel]}\n`
+                })
             }
-        }
-        // has media query..
-        else if ( mq && Object.entries(rules).length ) {
+        });
 
-            let mqPreview = ''
-            let mqOutput = ''
+        // build the output..
+        output.forEach((rules, mq) => {
 
-            for (const sel in rules) {
+            // console.log(mq, rules)
 
-                if ( !hasRule(sel, lookupMQ(mq)) ) {
-                    mqPreview += `  ${sel} ${rules[sel]}\n`
+            // no media query..
+            if ( !mq && Object.entries(rules).length ) {
+
+                for (const sel in rules) {
+
+                    if ( !hasRule(ssIndex, sel) ) {
+                        previewHTML += `${sel} ${rules[sel]}\n`
+                    }
+
+                    outputHTML += `${sel} ${rules[sel]}\n`
+                }
+            }
+            // has media query..
+            else if ( mq && Object.entries(rules).length ) {
+
+                let mqPreview = ''
+                let mqOutput = ''
+
+                for (const sel in rules) {
+
+                    if ( !hasRule(ssIndex, sel, lookupMQ(mq)) ) {
+                        mqPreview += `  ${sel} ${rules[sel]}\n`
+                    }
+
+                    mqOutput += `  ${sel} ${rules[sel]}\n`
                 }
 
-                mqOutput += `  ${sel} ${rules[sel]}\n`
+                if ( mqPreview ) {
+                    previewHTML += lookupMQ(mq) + ' {\n'
+                    previewHTML += mqOutput
+                    previewHTML += '}\n'
+                }
+
+                if ( mqOutput ) {
+                    outputHTML += lookupMQ(mq) + ' {\n'
+                    outputHTML += mqOutput
+                    outputHTML += '}\n'
+                }
+
             }
+        })
 
-            if ( mqPreview ) {
-                previewHTML += lookupMQ(mq) + ' {\n'
-                previewHTML += mqOutput
-                previewHTML += '}\n'
-            }
+        
+        // copy to clipboard the filtered missing styles only..
+        console.log('CSS:')
+        console.log(previewHTML)
+        copyToClipboard(previewHTML || '\n')
 
-            if ( mqOutput ) {
-                outputHTML += lookupMQ(mq) + ' {\n'
-                outputHTML += mqOutput
-                outputHTML += '}\n'
-            }
+        // write the css to the main selector..
+        selector.innerHTML = outputHTML
 
-        }
-    })
+    }
 
-    // write the css to the main selector..
-    console.log('CSS:')
-    console.log(previewHTML)
-    selector.innerHTML = outputHTML
+    function composeRule(match) {
 
+        let selector = '.' + escapeSelector(match[0]),
+            mq       = match[1] || '',
+            prop     = match[2],
+            whole    = match[3],
+            fraction = match[4] || '',
+            unit     = match[5];
 
+        const value = whole + fraction + unit
 
+        const rules = {
+
+            // padding
+            p:  `padding: ${value};`,
+            pt: `padding-top: ${value};`,
+            pr: `padding-right: ${value};`,
+            pb: `padding-bottom: ${value};`,
+            pl: `padding-left: ${value};`,
+            px: `padding-left: ${value}; padding-right: ${value};`,
+            py: `padding-top: ${value}; padding-bottom: ${value};`,
+
+            // margin
+            m:  `margin: ${value};`,
+            mt: `margin-top: ${value};`,
+            mr: `margin-right: ${value};`,
+            mb: `margin-bottom: ${value};`,
+            ml: `margin-left: ${value};`,
+            mx: `margin-left: ${value}; margin-right: ${value};`,
+            my: `margin-top: ${value}; margin-bottom: ${value};`,
+
+        };
+
+        const block = `{ ${rules[prop]} }`
+
+        return { mq, selector, block }
+
+    }
+
+    function copyToClipboard(text) {
+        const textarea = document.createElement('textarea');
+        document.body.appendChild(textarea);
+        textarea.value = text;
+        textarea.select();
+        textarea.setSelectionRange(0, 99999);
+        document.execCommand('copy');
+        document.body.removeChild(textarea);
+    }
 
     function getStyleSheetIndex(ss) {
         for ( let i=0; i < document.styleSheets.length; i++ ) {
@@ -135,7 +181,7 @@ function kickCSS(s, ssUrl='') {
         return null
     }
 
-    function hasRule(selectorText, conditionText='') {
+    function hasRule(ssIndex, selectorText, conditionText='') {
         if (ssIndex !== null) {
             const rules = document.styleSheets[ssIndex].rules
             for ( const rule of rules ) {
@@ -184,11 +230,15 @@ function kickCSS(s, ssUrl='') {
             pl: 'padding-left',
             pr: 'padding-right',
 
+            p: 'padding',
+
             // margin
             mt: 'margin-top',
             mb: 'margin-bottom',
             ml: 'margin-left',
             mr: 'margin-right',
+
+            m: 'margin',
 
             // widths
             mxw: 'max-width',
